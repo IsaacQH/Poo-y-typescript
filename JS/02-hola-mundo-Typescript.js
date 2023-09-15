8b35d75d3f2d0005170ceb4ae2a69caa499cda87
IsaacQH <isaacqwert@gmail.com>
2023-09-15 16:02:49 -0600

    02-hola-mundo-Typescript.ts: Completado y compilado a JS

---
+++ JS/02-hola-mundo-Typescript.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function () {
+    console.log("Hola mundo con Typescript"); //HOLA MUNDO USANDO TS
+    alert("Hola mundo con TS");
+});
---
+++ JS/02-hola-mundo-Typescript.ts
@@ -0,0 +1,7 @@
+
+window.addEventListener('load', () => {
+
+    console.log("Hola mundo con Typescript");         //HOLA MUNDO USANDO TS
+    alert("Hola mundo con TS");
+
+})
